b2a218067fe6a675b105a09e873af4a1e758a021
MarianoNavarrete <marianoskt14@hotmail.com>
2020-08-17 01:08:49 -0500

    consumeindo API

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,67 @@
 import React from 'react';
 import '../App.css';
+import axios from 'axios';
 
 
-function App() {
-  return (
-    <div className="App margen">
-      <table className = "tabla">
-        <thead>
-          <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Jose</td>
-            <td>jose@mail.com</td>
-            <td>jose.com</td>
-          </tr>
-          <tr>
-            <td>Ana</td>
-            <td>ana@mail.com</td>
-            <td>ana.com</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users:[]
+    }
+  }
+  async componentDidMount(){
+   const responseUsers = await axios.get('https://jsonplaceholder.typicode.com/users');
+   console.log(responseUsers);
+    this.setState(
+      {
+        users: responseUsers.data
+      }
+    );
+  }
+  
+  filas = () => (
+    this.state.users.map((usuario)=>(
+      
+      <tr key = {usuario.id}>
+        <td>
+          {usuario.name}
+        </td>
+        <td>
+          {usuario.email}
+        </td>
+        <td>
+          {usuario.website}
+        </td>
+      </tr>
+    ))
   );
+  render(){
+    console.log(this.state.users);
+    return (
+      <div className="App margen">
+        <table className = "tabla">
+          <thead>
+            <tr>
+              <th>
+                Nombre
+              </th>
+              <th>
+                Correo
+              </th>
+              <th>
+                Enlace
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.filas()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
